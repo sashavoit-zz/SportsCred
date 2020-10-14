9521dfaf33392321296294da80519563e3d685ee
YifeiGao <yiffygao@gmail.com>
2020-10-14 10:02:28 -0400

    create a new file called Sidebar, easy to import and use

--- front-end/src/components/dashboard/Dashboard.js
+++ front-end/src/components/dashboard/Dashboard.js
@@ -1,96 +1,15 @@
 // TODO: scrap this whole file
 
 import React from "react";
-import {
-  AppBar,
-  Drawer,
-  List,
-  ListItem,
-  ListItemIcon,
-  ListItemText,
-  Toolbar,
-  Typography,
-  makeStyles,
-} from "@material-ui/core";
-import InboxIcon from '@material-ui/icons/MoveToInbox';
-import MailIcon from '@material-ui/icons/Mail';
+import SideBar from "../SideBar/SideBar";
 
-const drawerWidth = 200;
-
-const useStyles = makeStyles((theme) => ({
-  root: {
-    display: "flex",
-  },
-  drawer: {
-    [theme.breakpoints.up("sm")]: {
-      width: drawerWidth,
-      flexShrink: 0,
-    },
-  },
-  appBar: {
-    [theme.breakpoints.up("sm")]: {
-      width: `calc(100% - ${drawerWidth}px)`,
-      marginLeft: drawerWidth,
-    },
-  },
-  toolbar: theme.mixins.toolbar,
-  drawerPaper: {
-    width: drawerWidth,
-    color: 'white',
-    backgroundColor: '#00000060',
-  },
-  content: {
-    flexGrow: 1,
-    padding: theme.spacing(3),
-  },
-}));
 
 function Dashboard(props) {
   const { children: component } = props;
-  const classes = useStyles();
-
-  const drawer = (
-    <div>
-      <div className={classes.toolbar} />
-      <List>
-        {["Inbox", "Starred", "Send email", "Drafts"].map((text, index) => (
-          <ListItem button key={text}>
-            <ListItemIcon>
-              {index % 2 === 0 ? <InboxIcon color="secondary" /> : <MailIcon color="secondary" />}
-            </ListItemIcon>
-            <ListItemText primary={text} />
-          </ListItem>
-        ))}
-      </List>
-    </div>
-  );
+  
 
   return (
-    <div className={classes.root}>
-      <AppBar position="fixed" color="secondary" className={classes.appBar}>
-        <Toolbar>
-          <Typography variant="h6" noWrap>
-            Title
-          </Typography>
-        </Toolbar>
-      </AppBar>
-      <nav className={classes.drawer} aria-label="mailbox folders">
-        {/* The implementation can be swapped with js to avoid SEO duplication of links. */}
-
-        <Drawer
-          classes={{
-            paper: classes.drawerPaper,
-          }}
-          variant="permanent"
-          open
-        >
-          {drawer}
-        </Drawer>
-      </nav>
-      <main className={classes.content}>
-         {component}
-      </main>
-    </div>
+    <SideBar page = "Dashboard"></SideBar>
   );
 }
 
