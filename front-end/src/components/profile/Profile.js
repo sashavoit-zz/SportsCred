cbfdffd1c6390f3c42144a4b5ad921b846df9ec5
KylSong <ssgxaa@gmail.com>
2020-10-14 17:57:26 -0400

    add form to edit profile

--- front-end/src/components/profile/Profile.js
+++ front-end/src/components/profile/Profile.js
@@ -8,6 +8,9 @@ import CardContent from '@material-ui/core/CardContent';
 import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
 
+import TextField from '@material-ui/core/TextField';
+import FaceIcon from '@material-ui/icons/Face';
+
 const useStyles = makeStyles({
   root: {
     position: "fixed",
@@ -19,20 +22,19 @@ const useStyles = makeStyles({
   },
   content: {
     overflow: "hidden", /* will contain if #first is longer than #second */
-
+    paddingTop: "80px",
   },
   menu: {
     float: "left",
     //display: "inline-block",
+    paddingTop: "40px",
     width: "30%",
     borderRight: "solid grey",
     textAlign: "center",
-    paddingTop: "40px",
   },
   option:{
     marginBottom:"30px"
   },
-
 
   profile: {
     overflow: "hidden",
@@ -40,21 +42,44 @@ const useStyles = makeStyles({
     //display: "inline-block",
   },
   leftProfile: {
-    marginLeft:"30px",
-    paddingTop: "40px",
+    //marginLeft:"30px",
+    //paddingTop: "40px",
     float: "left",
     width: "30%",
+    textAlign: "center",
     
     //height:"100%",
     //display: "inline-block",
   },
   rightProfile: {
-    paddingTop: "40px",
     overflow: "hidden",
     //height:"100%",
     //display: "inline-block",
   },
-  
+  bottomProfile: {
+    //overflow: "hidden",
+    position: "relative",
+    //height:"100%",
+    //display: "inline-block",
+    textAlign: "center",
+  },
+  userIcon:{
+    fontSize:"100px",
+  },
+  blueText:{
+    color:"#0099ff",
+    fontSize:"16px"
+  },
+  note:{
+    color:"#0d0d0d",
+    fontSize:"13px",
+    // width:"50%",
+    // textAlign: "center",
+  },
+  inputField:{
+    width:"50%",
+    marginTop:"20px",
+  }
 });
 
 
@@ -76,21 +101,38 @@ function SimpleCard() {
             </Typography>
           </div>
           <div className={classes.profile}>
-            <div className={classes.leftProfile}>
-              <Typography className={classes.option} variant="h5" component="h2">
-                Edsdofile
-              </Typography>
+            <div className={classes.profile}>
+              <div className={classes.leftProfile}>
+                <FaceIcon className={classes.userIcon}/>
+                <Typography className={classes.option} variant="h5" component="h2">
+                  Edsdofile
+                </Typography>
+              </div>
+              <div className={classes.rightProfile}>
+                <Typography variant="h5" component="h2">
+                  Me
+                </Typography>
+                <div className={classes.blueText}>
+                  Update Profile Picture
+                </div>
+              </div>
             </div>
-            <div className={classes.rightProfile}>
-              <Typography className={classes.option} variant="h5" component="h2">
-                Edit Profilesdsds
-              </Typography>
+            <div className={classes.bottomProfile}>
+              <TextField className={classes.inputField} id="outlined-basic" label="Name" variant="outlined"/><br/>
+              <div className={classes.note}>
+                Help people discover your account by using the name you're known by: either your full name, nickname, or business name
+              </div>
+              <TextField className={classes.inputField} id="outlined-basic" label="About" variant="outlined"/><br/>
+              
+              <TextField className={classes.inputField} id="outlined-basic" label="Email" variant="outlined"/><br/>
+              <TextField className={classes.inputField} id="outlined-basic" label="Phone Number" variant="outlined"/><br/>
+               <div className={classes.note}>
+                Personal Information (Email & Phone Number):
+                This wont be a part of your public profile.
+              </div>
             </div>
           </div>
         </CardContent>
-        <CardActions>
-          <Button size="small">Learn More</Button>
-        </CardActions>
       </Card>
     </SideBar>
   );
