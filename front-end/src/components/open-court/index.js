2b29fa9984c0d5e256de4ec38162fe9166bef62c
YifeiGao <yiffygao@gmail.com>
2020-10-15 16:51:46 -0400

    finishing the front end of uploading post

--- front-end/src/components/open-court/index.js
+++ front-end/src/components/open-court/index.js
@@ -1,20 +1,29 @@
 import React, { Component } from "react";
+import UploadPost from "./uploadPost"
 import Feed from "./feed"
 
 export class openCourt extends React.Component{
-    /* TODO:the posts in the state should fetch from the db, here are some dummy data */
+    
 
     constructor(props) {
         super(props);
         this.state = {
+            /**mock data for testing front end, will be deleted after finishing backend */
+            posts:[
+                {content:"mockcontent1mockcontent1",postTime:"27/08/2020",author:"mockauthor1", authorProfile: require("../../lib/profilePic.png"),likes:100, dislikes:29},
+                {content:"mockcontent2mockcontent2", postTime:"29/09/2020",author:"mockauthor2", authorProfile: require("../../lib/profilePic.png"),likes:30, dislikes:9}
+            ],
             /** TODO:The info of the currentUser should fetch from the db, here are some mock data */
-            currentUser:{userName:"mockCurrentUser", email:"mock@gmail.com", profilePic:require("../../lib/profilePic.png")}
+            currentUser:{userName:"mockUser", email:"mock@gmail.com", profilePic:require("../../lib/profilePic.png")},
         };
     }
 
     render(){
         return(
+            <div>
+            <UploadPost component = {this}/>
             <Feed />
+            </div>
         );
     }
 }
--- front-end/src/components/open-court/uploadPost.js
+++ front-end/src/components/open-court/uploadPost.js
@@ -1,20 +1,113 @@
-import React from 'react';
+import React from "react";
 import {withStyles} from "@material-ui/core/styles";
-
+import {TextField, Button, Avatar, Container, Grid, Typography} from '@material-ui/core'
 const styles = theme =>({
-    root:{
-        minWidth:800,
-        maxWidth:1000,
-    },
+    root: {
+        '& .MuiTextField-root': {
+             width:'100%',
+             margin:"auto",
+             display:"flex",
+         },
+
+      },
+      container:{
+          margin:'auto',
+          width:'60vw'
+      },
+      grid:{
+          flexGrow:1,
+      },
+      avatar:{
+          margin:'auto',
+      },
+      input:{
+          color:"white"
+      },
+      button:{
+          float:"right",
+          marginTop:`10px`,
+          margin: theme.spacing(3, 0, 2),
+      }
+
 });
 
-export class uploadPost extends React.Component{
-    render(){
-        const {currentUser} = this.props;
-        return(
-            <div></div>
-        );    
-    }
 
+
+export class UploadPost extends React.Component{
+    /* TODO:the posts in the state should fetch from the db, here are some dummy data */
+    state = {
+        uploadInput:""
+    };
+
+    render(){
+        const {component} = this.props;
+        const {classes} = this.props;
+        const handleInput=(component, field)=>{
+            const value = field.value;
+            const name = field.name;
+            component.setState({
+                [name]:value
+            })
+        }
+        const handleSubmit = (component) =>{
+            /**TODO: need to check if the user inputed or not. */
+            const posts = component.state.posts;
+            const today = new Date();
+            const date = today.getDate()+'/'+(today.getMonth()+1)+'/'+today.getFullYear();
+            console.log(date);
+            const newPost = {
+                content:this.state.uploadInput,
+                postTime:date,
+                author:component.state.currentUser.userName,
+                authorProfile:component.state.currentUser.profilePic,
+                likes:0,
+                dislikes:0
+            }
+            posts.push(newPost);
+            component.setState({
+                posts:posts
+            })
+        }
+        return(
+            <Container className = {classes.container}>
+                <div>
+                <form  className = {classes.root} noValidate autoComplete = "off">
+                <div>
+                    <Grid className = {classes.grid} container spacing={1}>
+                        <Grid item >
+                            <Avatar className={classes.avatar} alt="user profile" src={component.state.currentUser.profilePic}/>
+                            <Typography>{component.state.currentUser.userName}</Typography>                        
+                        </Grid>
+                        <Grid item xs>
+                            <TextField 
+                            id = "standard-multiline-static"
+                            multiline
+                            name = "uploadInput"
+                            // value={value}
+                            onChange={e => handleInput(this,e.target)}
+                            variant="outlined"
+                            placeholder="What's in your mind?"
+                            InputProps={
+                                {className: classes.input}
+                            }
+                            />
+                        </Grid>
+                    
+                    </Grid>
+                    <Button 
+                    className ={classes.button}
+                    variant="contained" 
+                    color="primary"
+                    onClick={e=>handleSubmit()}
+                    >
+                        Submit
+                    </Button>
+                </div>
+            </form>
+                </div>
+                
+            </Container>
+        );
+    }
 }
-export default withStyles(styles)(uploadPost);
+export default withStyles(styles)(UploadPost);
